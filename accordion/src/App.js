daae76c2267b569c79396ec2c77a591c23ac20ef
thelaserunicorn <rashideram2@gmail.com>
2021-07-27 10:15:33 +0600

    accordion

---
+++ accordion/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <div className="container">
+      <h3>Frequently Asked Questions</h3>
+      <section className='info'>
+        {
+          questions.map((question) => {
+            return <SingleQuestion key ={question.id}{...question}/>
+          })
+        }
+      </section>
+    </div>
+  )
+}
+
+export default App;
