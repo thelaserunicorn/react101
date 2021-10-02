df4c08e0dc39489e5c8d710c729dcf0fa171c485
thelaserunicorn <rashideram2@gmail.com>
2021-10-02 14:41:28 +0600

    added-README

--- movie-app-api/README.md
+++ movie-app-api/README.md
@@ -1,3 +1,4 @@
+# [LIVE URL](https://movies-favourites-react.netlify.app/) 
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- movie-app-api/src/components/SearchBox.js
+++ movie-app-api/src/components/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBox = () => {
     return (
         <div className="col col-sm-4">
-            <input className="form-control"></input>
+            <input className="form-control" placeholder="Search Movies"></input>
         </div>
     )
 }
